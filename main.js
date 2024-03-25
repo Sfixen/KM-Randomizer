8aeca312de410ad68864108bfab5ddb6dbef82ab
Sfixen <164667169+Sfixen@users.noreply.github.com>
2024-03-25 22:11:45 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -20,15 +20,22 @@
         let mods = [];
         let val = 0;
         let att = 0;
+
         while(val != max && att < 100){
             mods = [];
             val = 0;
             let modValRand = [...modVal];
             let modNameRand = [...modName];
+            for(let i=0; i < modVal.length; i++){
+              if (includeList.includes(modName[i])){
+                val += modVal[i];
+                mods.push(modName[i]);
+              }
+            }
             while(val < max && modValRand.length > 0){
                 let x = Math.floor(Math.random() * modValRand.length);
   
-                if(excludeList.includes(modNameRand[x]) == false){
+                if(excludeList.includes(modNameRand[x]) == false && includeList.includes(modNameRand[x]) == false){
                     if(val + modValRand[x] <= max){
                     val += modValRand[x];
                     mods.push(modNameRand[x]);
@@ -40,20 +47,35 @@
             att++;
         }
         if(att < 100){
-            return mods;
+          let modIDs = [];
+
+          for(let i=0; i < mods.length; i++){
+            modIDs.push(modName.indexOf(mods[i]))
+          }
+          console.log(modIDs);
+          return modIDs;
         }else{
-            return false;
+          return "false";
         }
       }
   
       function randomize(){
+        clearColor();
         bigCheck();
         let max = document.getElementById("points").value;
         let drawnMods = randMods(max);
-        if(drawnMods==false){
-            document.getElementById("display").innerHTML = "Choose a different point value";
+        console.log(drawnMods)
+        if(drawnMods=="false"){
+            document.getElementById("pointValue").innerHTML = "Choose a different point value!";
         }else{
-            document.getElementById("display").innerHTML = drawnMods.join(" | ");
+
+          for(let i=0; i < drawnMods.length; i++){
+            applyStyles("#F8FFD1", "invert(93%) sepia(11%) saturate(589%) hue-rotate(28deg) brightness(105%) contrast(103%)", drawnMods[i]);
+            console.log("done")
+          }
+
+          document.getElementById("pointValue").innerHTML = " ";
+          console.log("displayed!");
         }
       }
   
@@ -68,10 +90,9 @@
                 excludeList.push(modName[id]);
                 break;
               case "1":
-                console.log("Included" + radioID);
+                includeList.push(modName[id]);
                 break;
               case "2":
-                console.log("None" + radioID);
                 break;
             }
             break;
@@ -81,11 +102,78 @@
   
       function bigCheck(){
         excludeList = [];
+        includeList = [];
         for(let i=0; i < 15; i++){
           check(i);
         }
       }
+
+      function applyStyles(color, filter, id) {
+        // Select elements with class 'mod'
+        const modElements = document.querySelectorAll(`.mod${id}`);
+        
+        // Apply styles to elements with class 'mod'
+        modElements.forEach(element => {
+            element.style.color = color;
+            element.style.borderColor = color;
+        });
+    
+        // Select img elements within td.mod under tr.icon
+        const imgElements = document.querySelectorAll(`tr > td.mod${id} > img`);
+        
+        // Apply filter style to img elements
+        imgElements.forEach(element => {
+            element.style.filter = filter;
+        });
+    }
+
+      function radioColor(id){
+        clearColor();
+        radioID = "radio" + id;
+        let radioOptions = document.getElementsByName(radioID);
+  
+        for (let i = 0; i < radioOptions.length; i++){
+          if (radioOptions[i].checked){
+            switch(radioOptions[i].value){
+              case "0":
+                applyStyles("#511F3F", "invert(13%) sepia(14%) saturate(6533%) hue-rotate(288deg) brightness(88%) contrast(89%)", id);
+                break;
+              case "1":
+                applyStyles("#F8FFD1", "invert(93%) sepia(11%) saturate(589%) hue-rotate(28deg) brightness(105%) contrast(103%)", id);
+                break;
+              case "2":
+                applyStyles("#A32632", "invert(16%) sepia(78%) saturate(2313%) hue-rotate(335deg) brightness(102%) contrast(92%)", id);
+                break;
+            }
+            break;
+          }
+        }
+      }
+
+      function clearColor(){
+        for(let i=0; i < modName.length; i++){
+          let radioOptions = document.getElementsByName(`radio${i}`);
+          if(radioOptions[2].checked)
+            applyStyles("#A32632", "invert(16%) sepia(78%) saturate(2313%) hue-rotate(335deg) brightness(102%) contrast(92%)", i);
+        }
+      }
+
+      //WORKS BUT SOME WEIRD ERROR
+      /*
+      function clearButton(){
+          var radios = document.querySelectorAll('input[type="radio"]');
+
+          for (let i=0; i < radios.length; i++) {
+              radios[i].checked = false;
+          }
+
+          for(let i=0; i < radios.length; i++){
+            document.querySelector(`input[name="radio${i}"][value="2"]`).checked = true;
+          }
+      }
+      */
   
       let modVal = [5, 5, 15, 20, 15, 30, 20, 5, 5, 20, 15, 15, 10, 50, 20];
       let modName = ["No Hook", "No Clover", "Boss Totems", "All Totem Battles", "More Difficult I", "Single Candle", "Squirrel Fish", "Smaller Backpack", "Pricey Pelts", "Tipped Scales", "No Boss Rares", "More Difficult II", "Annoying Starters", "Grizzly Bosses", "Final Boss"];
       let excludeList = [];
+      let includeList = [];
